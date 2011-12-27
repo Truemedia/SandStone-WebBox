05030bae186d8cf28ca8e3d214099f1f43094fdf
Wade Penistone <truemedia@mediacityonline.net>
2011-12-27 12:34:19 +0000

    added comments only madatory sql file

---
+++ install/database/mysql/mandatory_data.sql
@@ -0,0 +1,6 @@
+// data to be installed for the software to work at all
+
+// insert this softwares details
+/* to be finalized once field datatypes have been set properly */
+
+// insert data tied to other tables but related to this software */
